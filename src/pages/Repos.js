1913c03395a01e34ee597c1ea1b07fdb506a84b7
Oshoke Oyati <oyatioshoke@yahoo.com>
2022-11-04 07:45:34 +0100

    loading state, single repo and responsiveness

--- src/pages/Repos.js
+++ src/pages/Repos.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
 
 const Repos = () => {
   const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const url = 'https://api.github.com/users/shokes/repos';
 
   const getRepos = async () => {
+    setLoading(true);
     const response = await fetch(url);
     const data = await response.json();
 
+    setLoading(false);
     setRepos(data);
   };
 
@@ -19,26 +23,70 @@ const Repos = () => {
   }, []);
 
   return (
-    <section className='container'>
-      <h2 className='heading'>Repositories</h2>
+    <>
+      <head>
+        <title>Github Portfolio</title>
+        <meta
+          name='description'
+          content='Github repositories of Github User, Oshoke Oyati'
+        />
+        <meta property='og:title' content='Github Portfolio' />
+        <meta
+          property='og:description'
+          content='Github repositories of Github User, Oshoke Oyati.'
+        />
 
-      <div>
-        <ul className='repos'>
-          {repos.map((repo) => {
-            const { name, id, description, language } = repo;
-            return (
-              <Link key={id} className='repo' to={`/${name}`}>
-                <h2 className='name'> {name}</h2>
-                <p className='description'>
-                  {description || 'No description available at the moment'}
-                </p>
-                <span>{language}</span>
-              </Link>
-            );
-          })}
-        </ul>
-      </div>
-    </section>
+        {/* change after deploument */}
+        <meta property='og:url' content='https://oshoke.vercel.app/' />
+        <meta property='og:type' content='website' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+      </head>
+
+      <section className='container'>
+        <head>
+          <title>Github Portfolio</title>
+          <meta
+            name='description'
+            content='Github repositories of Github User, Oshoke Oyati'
+          />
+          <meta property='og:title' content='Github Portfolio' />
+          <meta
+            property='og:description'
+            content='Github repositories of Github User, Oshoke Oyati.'
+          />
+
+          {/* change after deploument */}
+          <meta property='og:url' content='https://oshoke.vercel.app/' />
+          <meta property='og:type' content='website' />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        </head>
+
+        <h2 className='heading'>Repositories</h2>
+
+        <div>
+          {loading ? (
+            <Loading />
+          ) : (
+            <div className='repos'>
+              {repos.map((repo) => {
+                const { name, id, description, language } = repo;
+                return (
+                  <Link key={id} className='repo' to={`/${name}`}>
+                    <h2 className='name'> {name}</h2>
+                    <p className='description'>
+                      {description || 'No description available at the moment'}
+                    </p>
+                    <span>{language}</span>
+                  </Link>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/pages/SingleRepo.js
+++ src/pages/SingleRepo.js
@@ -1,18 +1,20 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
 
 const SingleRepo = () => {
   const { name } = useParams();
   const [repo, setRepo] = useState(null);
-  console.log(repo);
+  const [loading, setLoading] = useState(true);
 
   const url = `https://api.github.com/repos/shokes/${name}`;
 
   const getSingleRepo = async () => {
+    setLoading(true);
     const response = await fetch(url);
     const data = await response.json();
-
+    setLoading(false);
     setRepo(data);
   };
 
@@ -22,19 +24,75 @@ const SingleRepo = () => {
   }, [name]);
 
   if (repo) {
-    const { description, topics } = repo;
+    const {
+      description,
+
+      default_branch: branch,
+      stargazers_count: stars,
+      visibility,
+      language,
+      html_url: url,
+    } = repo;
 
     return (
-      <section className='container'>
-        <Link to='/' className='back'>
-          <span>back to repos</span>
-        </Link>
+      <>
+        <head>
+          <title>{name}</title>
+          <meta name='description' content={`${name}`} />
+          <meta property='og:title' content={`${name}`} />
+          <meta property='og:description' content={`${name}`} />
 
-        <div className='single-repo'>
-          <h2>{name}</h2>
-          <p>{description}</p>
-        </div>
-      </section>
+          {/* change after deploument */}
+          <meta property='og:url' content='https://oshoke.vercel.app/' />
+          <meta property='og:type' content='website' />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        </head>
+
+        <section className='container'>
+          <Link to='/' className='back'>
+            <span>back to repos</span>
+          </Link>
+
+          {loading ? (
+            <Loading />
+          ) : (
+            <div className='single-repo'>
+              <h2 className='single-name'>{name}</h2>
+              <p className='single-desc'>
+                {description || 'description not available'}
+              </p>
+              <div>
+                <span>Stars: {stars}</span>
+              </div>
+              <div>
+                <span className='cap'>Visibility: {visibility}</span>
+              </div>
+              <div>
+                <span className='cap'>
+                  Code:{' '}
+                  <a
+                    href={url}
+                    className='url'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    github
+                  </a>
+                </span>
+              </div>
+              <div>
+                <span className='cap'>
+                  Main language: {language || 'not available'}
+                </span>
+              </div>
+              <div>
+                <span className='cap'>Branch: {branch}</span>
+              </div>
+            </div>
+          )}
+        </section>
+      </>
     );
   } else {
     return;
