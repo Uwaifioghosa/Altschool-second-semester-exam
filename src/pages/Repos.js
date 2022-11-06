048165519e0d449794961f3a096c15c9daadf726
Osezei <john.osezei@yahoo.com>
2022-11-06 21:56:45 +0100

    first commit

--- src/pages/Repos.js
+++ src/pages/Repos.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Loading from '../components/Loading';
-import ErrorBoundary from '../components/ErrorBoundary';
-import OurFallbackComponent from '../components/FallBack';
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Loading from "../components/Loading";
+import ErrorBoundary from "../components/ErrorBoundary";
+import OurFallbackComponent from "../components/FallBack";
 
 const Repos = () => {
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const url = 'https://api.github.com/users/shokes/repos';
+  const url = "https://api.github.com/users/uwaifioghosa/repos";
 
   const getRepos = async () => {
     setLoading(true);
@@ -29,39 +29,39 @@ const Repos = () => {
       <head>
         <title>Github Portfolio</title>
         <meta
-          name='description'
-          content='Github repositories of Github User, Oshoke Oyati'
+          name="description"
+          content="Github repositories of Github User, Oshoke Oyati"
         />
-        <meta property='og:title' content='Github Portfolio' />
+        <meta property="og:title" content="Github Portfolio" />
         <meta
-          property='og:description'
-          content='Github repositories of Github User, Oshoke Oyati.'
+          property="og:description"
+          content="Github repositories of Github User, Oshoke Oyati."
         />
 
         <meta
-          property='og:url'
-          content='https://g-ithub-user-examination.vercel.app/'
+          property="og:url"
+          content="https://g-ithub-user-examination.vercel.app/"
         />
-        <meta property='og:type' content='website' />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <meta property="og:type" content="website" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
       </head>
       <ErrorBoundary FallbackComponent={OurFallbackComponent}>
-        <section className='container'>
-          <h2 className='heading'>Repositories</h2>
+        <section className="container">
+          <h2 className="heading">Repositories</h2>
 
           <div>
             {loading ? (
               <Loading />
             ) : (
-              <div className='repos'>
+              <div className="repos">
                 {repos.map((repo) => {
                   const { name, id, description, language } = repo;
                   return (
-                    <Link key={id} className='repo' to={`/${name}`}>
-                      <h2 className='name'> {name}</h2>
-                      <p className='description'>
+                    <Link key={id} className="repo" to={`/${name}`}>
+                      <h2 className="name"> {name}</h2>
+                      <p className="description">
                         {description ||
-                          'No description available at the moment'}
+                          "No description available at the moment"}
                       </p>
                       <span>{language}</span>
                     </Link>
--- src/pages/SingleRepo.js
+++ src/pages/SingleRepo.js
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import Loading from '../components/Loading';
-import ErrorBoundary from '../components/ErrorBoundary';
-import OurFallbackComponent from '../components/FallBack';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Loading from "../components/Loading";
+import ErrorBoundary from "../components/ErrorBoundary";
+import OurFallbackComponent from "../components/FallBack";
 
 const SingleRepo = () => {
   const { name } = useParams();
   const [repo, setRepo] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const url = `https://api.github.com/repos/shokes/${name}`;
+  const url = `https://api.github.com/repos/uwaifioghosa/${name}`;
 
   const getSingleRepo = async () => {
     setLoading(true);
@@ -40,57 +40,57 @@ const SingleRepo = () => {
       <>
         <head>
           <title>{name}</title>
-          <meta name='description' content={`${name}`} />
-          <meta property='og:title' content={`${name}`} />
-          <meta property='og:description' content={`${name}`} />
+          <meta name="description" content={`${name}`} />
+          <meta property="og:title" content={`${name}`} />
+          <meta property="og:description" content={`${name}`} />
 
           <meta
-            property='og:url'
+            property="og:url"
             content={`https://g-ithub-user-examination.vercel.app/${name}`}
           />
-          <meta property='og:type' content='website' />
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <meta property="og:type" content="website" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
         </head>
         <ErrorBoundary FallbackComponent={OurFallbackComponent}>
-          <section className='container'>
-            <Link to='/' className='back'>
+          <section className="container">
+            <Link to="/" className="back">
               <span>back to repos</span>
             </Link>
 
             {loading ? (
               <Loading />
             ) : (
-              <div className='single-repo'>
-                <h2 className='single-name'>{name}</h2>
-                <p className='single-desc'>
-                  {description || 'description not available'}
+              <div className="single-repo">
+                <h2 className="single-name">{name}</h2>
+                <p className="single-desc">
+                  {description || "description not available"}
                 </p>
                 <div>
                   <span>Stars: {stars}</span>
                 </div>
                 <div>
-                  <span className='cap'>Visibility: {visibility}</span>
+                  <span className="cap">Visibility: {visibility}</span>
                 </div>
                 <div>
-                  <span className='cap'>
-                    Code:{' '}
+                  <span className="cap">
+                    Code:{" "}
                     <a
                       href={url}
-                      className='url'
-                      target='_blank'
-                      rel='noreferrer'
+                      className="url"
+                      target="_blank"
+                      rel="noreferrer"
                     >
                       github
                     </a>
                   </span>
                 </div>
                 <div>
-                  <span className='cap'>
-                    Main language: {language || 'not available'}
+                  <span className="cap">
+                    Main language: {language || "not available"}
                   </span>
                 </div>
                 <div>
-                  <span className='cap'>Branch: {branch}</span>
+                  <span className="cap">Branch: {branch}</span>
                 </div>
               </div>
             )}
